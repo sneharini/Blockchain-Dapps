e79c3ef06f2a9eb9e7672921946b521d63e38dfc
sneharini <Leviosa@2001>
2023-06-27 15:36:07 +0530

    Build an own cryptocurrency on Ethereum

--- contracts/LW3Token.sol
+++ contracts/LW3Token.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+//import ERC20 contract from OpenZeppelin
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract LW3Token is ERC20 {
